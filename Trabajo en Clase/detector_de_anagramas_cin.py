99c3b9f413612e25f1c381465192101aacd99d5a
cinvilla <cylivralaj27@gmail.com>
2018-11-10 18:11:33 -0600

    Detector de anagramas hecho por profe

--- Trabajo en Clase/detector_de_anagramas_cin.py
+++ Trabajo en Clase/detector_de_anagramas_cin.py
@@ -1,14 +1,34 @@
-def anag_dect(pal1,pal2):
-    if len(pal1) == len(pal2):
-        return True
-    else:
-        return False
+# CÓDIGO ERRÓNEO DEBIDO A QUE SI SE EVALUABA EL # DE
+# CARACTERES SE CAÍA EN EL RIESGO DE QUE PALABRAS CON
+# DIFERENTES LETRAS DIERAN TRUE SOLO POR EL # DE LETRAS....
+# def anag_dect(pal1,pal2):
+#     if len(pal1) == len(pal2):
+#         return True
+#     else:
+#         return False
+#
+# a = 'car'
+# b = 'arc'
+# print(anag_dect(a,b))
+#
+# a = 'ana'
+# b = 'een'
+# print()
+# print(anag_dect(a,b))
 
-a = 'car'
-b = 'arc'
-print(anag_dect(a,b))
+# EVALUACIÓN HECHA POR LE PROFESOR utilizando sorted para ordenar las palabras y
+# presentarlas en igualdad
+def anagrama_orden(primera, segunda):
+    return sorted(primera) == sorted(segunda)
 
-a = 'ana'
-b = 'een'
-print()
-print(anag_dect(a,b))
+print(anagrama_orden('roma','amor'))
+assert False == anagrama_orden('roma','amor'), 'Este es un anagrama'
+
+
+
+
+
+
+
+
+
